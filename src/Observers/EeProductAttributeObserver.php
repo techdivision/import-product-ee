097163aec4988f819804dd1ebe324417e41beadf
wagnert <wagner_tim78@hotmail.com>
2016-12-08 16:02:31 +0100

    Documentation tasks

--- src/Observers/EeProductAttributeObserver.php
+++ src/Observers/EeProductAttributeObserver.php
@@ -11,11 +11,11 @@
  *
  * PHP version 5
  *
- * @author    Tim Wagner <tw@appserver.io>
- * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/wagnert/csv-import
- * @link      http://www.appserver.io
+ * @link      https://github.com/techdivision/import-product-ee
+ * @link      http://www.techdivision.com
  */
 
 namespace TechDivision\Import\Product\Ee\Observers;
@@ -25,55 +25,23 @@ use TechDivision\Import\Product\Observers\ProductAttributeObserver;
 /**
  * A SLSB that handles the process to import product bunches.
  *
- * @author    Tim Wagner <tw@appserver.io>
- * @copyright 2015 TechDivision GmbH <info@appserver.io>
+ * @author    Tim Wagner <t.wagner@techdivision.com>
+ * @copyright 2016 TechDivision GmbH <info@techdivision.com>
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
- * @link      https://github.com/wagnert/csv-import
- * @link      http://www.appserver.io
+ * @link      https://github.com/techdivision/import-product-ee
+ * @link      http://www.techdivision.com
  */
 class EeProductAttributeObserver extends ProductAttributeObserver
 {
 
     /**
-     * This method finally persists the passed value by invoking the
-     * persist method defined by the attribute's backend type.
+     * Return's the PK to create the product => attribute relation.
      *
-     * @param mixed $value The value to persist
-     *
-     * @return void
+     * @return integer The PK to create the relation with
      */
-    public function processAttribute($value)
+    public function getPrimaryKey()
     {
-
-        // laod the callbacks for the actual attribute code
-        $callbacks = $this->getCallbacksByType($this->getAttributeCode());
-
-        // invoke the pre-cast callbacks
-        foreach ($callbacks as $callback) {
-            $value = $callback->handle($value);
-        }
-
-        // load the row ID of the product that has been created recently
-        $lastRowId = $this->getLastRowId();
-
-        // load the ID of the attribute to create the values for
-        $attributeId = $this->getAttributeId();
-
-        // load the store ID
-        $storeId = $this->getRowStoreId();
-
-        // load the backend type of the actual attribute
-        $backendType = $this->getBackendType();
-
-        // cast the value based on the backend type
-        $castedValue = $this->castValueByBackendType($backendType, $value);
-
-        // prepare the attribute values
-        $attribute = array($lastRowId, $attributeId, $storeId, $castedValue);
-
-        // initialize and persist the entity attribute
-        $persistMethod = $this->getPersistMethod();
-        $this->$persistMethod($attribute);
+        return $this->getLastRowId();
     }
 
     /**
