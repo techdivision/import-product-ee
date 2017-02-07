c933a590f8e947716f1444c73669f4c7c0799a4b
wagnert <wagner_tim78@hotmail.com>
2017-02-07 16:42:06 +0100

    Optimisations to use protected variables instead of methods

--- src/Observers/EeProductAttributeObserverTrait.php
+++ src/Observers/EeProductAttributeObserverTrait.php
@@ -43,37 +43,28 @@ trait EeProductAttributeObserverTrait
     protected function prepareAttributes()
     {
 
-        // load the attribute value
-        $attributeValue = $this->getAttributeValue();
-
         // laod the callbacks for the actual attribute code
-        $callbacks = $this->getCallbacksByType($this->getAttributeCode());
+        $callbacks = $this->getCallbacksByType($this->attributeCode);
 
         // invoke the pre-cast callbacks
         foreach ($callbacks as $callback) {
-            $attributeValue = $callback->handle($attributeValue);
+            $this->attributeValue = $callback->handle($this->attributeValue);
         }
 
         // load the ID of the product that has been created recently
         $lastEntityId = $this->getPrimaryKey();
 
-        // load the ID of the attribute to create the values for
-        $attributeId = $this->getAttributeId();
-
         // load the store ID
         $storeId = $this->getRowStoreId(StoreViewCodes::ADMIN);
 
-        // load the backend type of the actual attribute
-        $backendType = $this->getBackendType();
-
         // cast the value based on the backend type
-        $castedValue = $this->castValueByBackendType($backendType, $attributeValue);
+        $castedValue = $this->castValueByBackendType($this->backendType, $this->attributeValue);
 
         // prepare the attribute values
         return $this->initializeEntity(
             array(
                 MemberNames::ROW_ID       => $lastEntityId,
-                MemberNames::ATTRIBUTE_ID => $attributeId,
+                MemberNames::ATTRIBUTE_ID => $this->attributeId,
                 MemberNames::STORE_ID     => $storeId,
                 MemberNames::VALUE        => $castedValue
             )
