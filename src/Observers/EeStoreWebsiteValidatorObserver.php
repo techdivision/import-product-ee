e5d97d23c0122afa7e35d4d53e4aacbbcd832f2a
yamlahik <k.yamlahi@techdivision.com>
2024-09-05 15:22:27 +0200

    PAC-309: compatibility code

--- src/Observers/EeStoreWebsiteValidatorObserver.php
+++ src/Observers/EeStoreWebsiteValidatorObserver.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * TechDivision\Import\Callbacks\ArrayValidatorCallback
+ * TechDivision\Import\Product\Ee\Observers\StoreWebsiteValidatorObserver
  *
  * PHP version 7
  *
@@ -19,7 +19,7 @@ use TechDivision\Import\Product\Observers\StoreWebsiteValidatorObserver;
 use TechDivision\Import\Product\Utils\MemberNames;
 
 /**
- * storeview validator callback implementation.
+ * Store view validator implementation.
  *
  * @author    MET <met@techdivision.com>
  * @copyright 2024 TechDivision GmbH <info@techdivision.com>
@@ -32,10 +32,10 @@ class EeStoreWebsiteValidatorObserver extends StoreWebsiteValidatorObserver
     /**
      * @return void
      */
-    public function setLastEntityRowId(): void
+    public function setLastEntityRowId($sku): void
     {
-        if (!$this->hasBeenProcessed($this->getValue(ColumnKeys::SKU))) {
-            $this->entity = $this->loadProduct($this->getValue(MemberNames::SKU));
+        if (!$this->hasBeenProcessed($sku)) {
+            $this->entity = $this->loadProduct($sku);
             $this->setLastEntityId($this->entity[MemberNames::ROW_ID]);
             $this->lastEntityId = $this->getLastEntityId();
         }
